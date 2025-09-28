77e2e5962ce9b7b96a83c945771507a45f6a4a36
Jefferson Smith <Jeffersonneilsmith@gmail.com>
2025-09-28 03:57:43 -0400

    Introduce a code view with syntax highlighting and Github Dark theme functionality. Added a file explorer with a tree view, navigation and copy to clipboard functionality for code files. the project view has been enhanced with tab navigation allowing users to switch between a live demo and code view of project fragments.

--- src/modules/projects/ui/views/project-view.tsx
+++ src/modules/projects/ui/views/project-view.tsx
@@ -1,6 +1,9 @@
 "use client";
 
-import { Suspense } from "react";
+import { Suspense, useState } from "react";
+
+import { Fragment } from "@/generated/prisma"; 
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"; 
 
 import {
     ResizableHandle,
@@ -8,6 +11,12 @@ import {
     ResizablePanelGroup,
 } from "@/components/ui/resizable"; 
 import { MessagesContainer } from "../components/messages-container";
+import { ProjectHeader } from "../components/project-header";
+import { FragmentWeb } from "../components/fragment-web"; 
+import { EyeIcon, CodeIcon, CrownIcon } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import { FileExplorer } from "@/components/file-explorer";
  
 
 interface Props {
@@ -15,6 +24,9 @@ interface Props {
 };
 
 export const ProjectView = ({ projectId }: Props) => {
+  const [activeFragment, setActiveFragment] = useState<Fragment | null>(null);
+  const [tabState, setTabState] = useState<"preview" | "code">("preview");
+
 return (
     <div className="h-screen">
         <ResizablePanelGroup direction="horizontal">
@@ -23,8 +35,15 @@ return (
           minSize={20}
           className="flex flex-col min-h-0"
         >
+          <Suspense fallback={<p>Loading project...</p>}>
+            <ProjectHeader projectId={projectId}/>
+          </Suspense>
         <Suspense fallback={<p>Loading messages...</p>}>
-           <MessagesContainer projectId="{projectId}" />
+           <MessagesContainer 
+           projectId={projectId} 
+           activeFragment={activeFragment}
+           setActiveFragment={setActiveFragment}
+           />
         </Suspense> 
         </ResizablePanel>
         <ResizableHandle withHandle />
@@ -32,7 +51,40 @@ return (
           defaultSize={65}
           minSize={50}
         >
-        TODO: Preview
+        <Tabs
+        className="h-full gap-y-0"
+        defaultValue="preview"
+        value={tabState}
+        onValueChange={(value) => setTabState(value as "preview" | "code")}
+        >
+        <div className="w-full flex items-center p-2 border-b gap-x-2">
+          <TabsList className="h-8 p-0 border rounded-md">
+            <TabsTrigger value="preview" className="rounded-md">
+              <EyeIcon /> <span>Demo</span>
+            </TabsTrigger>
+            <TabsTrigger value="code" className="rounded-md">
+              <CodeIcon /> <span>Code</span>
+            </TabsTrigger>
+          </TabsList>
+          <div className="ml-auto flex items-center gap-x-2">
+            <Button asChild size="sm" variant="default">
+              <Link href="/pricing">
+                <CrownIcon /> Upgrade
+              </Link>
+            </Button>
+          </div>
+        </div>
+        <TabsContent value="preview">
+            { !!activeFragment && <FragmentWeb data={activeFragment} />}
+        </TabsContent>
+        <TabsContent value="code" className="min-h-0">
+         {!!activeFragment?.files && (
+          <FileExplorer 
+             files={activeFragment.files as { [path: string]: string}}
+          />
+         )}
+        </TabsContent>
+        </Tabs>
         </ResizablePanel>
         </ResizablePanelGroup>
      </div> 
