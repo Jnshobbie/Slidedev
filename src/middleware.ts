585b4f229e222e6a9fe56f29c4f27aef185ebe24
Jefferson Smith <Jeffersonneilsmith@gmail.com>
2026-09-02 14:34:54 -0400

    discovery route added to clerk middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,6 +6,7 @@ const isPublicRoute = createRouteMatcher([
     "/sign-up(.*)",
     "/api(.*)",
     "/pricing(.*)",
+    "/.well-known(.*)",
 ]);
 
 export default clerkMiddleware(async (auth, req) => {
